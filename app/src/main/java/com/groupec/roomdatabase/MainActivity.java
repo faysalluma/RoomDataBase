7ab5ca77d2de30f5fd5a8e7249265c3d4cf1d192
Faysal SANDA <faysalgluma@gmail.com>
2021-11-03 12:28:00 +0100

    Initialise Repository

---
+++ app/src/main/java/com/groupec/roomdatabase/MainActivity.java
@@ -0,0 +1,71 @@
+package com.groupec.roomdatabase;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.groupec.roomdatabase.adapters.TasksAdapter;
+import com.groupec.roomdatabase.databases.DatabaseClient;
+import com.groupec.roomdatabase.entities.Task;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton buttonAddTask;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerview_tasks);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        buttonAddTask = findViewById(R.id.floating_button_add);
+        buttonAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddTaskActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        getTasks();
+
+    }
+
+    private void getTasks() {
+        class GetTasks extends AsyncTask<Void, Void, List<Task>> {
+
+            @Override
+            protected List<Task> doInBackground(Void... voids) {
+                List<Task> taskList = DatabaseClient
+                        .getInstance(getApplicationContext())
+                        .getAppDatabase()
+                        .taskDao()
+                        .getAll();
+                return taskList;
+            }
+
+            @Override
+            protected void onPostExecute(List<Task> tasks) {
+                super.onPostExecute(tasks);
+                TasksAdapter adapter = new TasksAdapter(MainActivity.this, tasks);
+                recyclerView.setAdapter(adapter);
+            }
+        }
+
+        GetTasks gt = new GetTasks();
+        gt.execute();
+    }
+
+}
